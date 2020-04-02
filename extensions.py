33f71f61a784e85e4b9de0c0b011f47ce33aa931
Saltpile123 <ikhoudvanberichten@gmail.com>
2020-04-02 23:02:03 +0200

    enqueue toegevoegd

--- extensions.py
+++ extensions.py
@@ -54,7 +54,7 @@ class SpotifyExtension(Extension):
       succeeded = True
 
     except SpotifyException as ex:
-      YLogger.exception_nostack(context, "Spotify exception raised", ex)
+      YLogger.exception(context, "Spotify exception raised", ex)
       message = str(ex).splitlines()[1].strip()
       #YLogger.exception_nostack(context, "Spotify exception raised", message)
 
@@ -66,11 +66,11 @@ class SpotifyExtension(Extension):
       fail = f"PYFAIL SPOTIFYY {code} {params[0].replace('_', '').upper()}"
 
     except (NameError, AttributeError, SyntaxError) as ex:
-      YLogger.exception_nostack(context, f"Couldn't evaluate command {cmd}", ex)
+      YLogger.exception(context, f"Couldn't evaluate command {cmd}", ex)
       fail = f"PYFAIL INVALIDCOMMAND {params[0].replace('_', '').upper()}"
 
     except Exception as ex:
-      YLogger.exception_nostack(context, f"Spotify extension fail with {cmd}", ex)
+      YLogger.exception(context, f"Spotify extension fail with {cmd}", ex)
       fail = "PYFAIL ERROR"
 
     with open('session.pickle', 'wb') as file:
