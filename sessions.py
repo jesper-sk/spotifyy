55da94312824131e9985db5effbb03df07619684
Saltpile123 <ikhoudvanberichten@gmail.com>
2020-04-03 19:59:33 +0200

    nou lijkt me wel genoeg help

--- sessions.py
+++ sessions.py
@@ -42,7 +42,27 @@ class SpotifySession():
   def help(self):
     print("Hello I am SpotBot.")
     print("I am able to have a conversation with you and control your Spotify.")
+    print("In all the help functions I will refer to commands you can give to me between brackets, like [pause].")
+    print("")
     print("For me to work optimally I want you to be a friendly person and always speak in multiple words, except when I ask very specific questions like yes/no-questions.")
+    print("If I still don't understand you, you may need to be more specific in your answer, e.g.:")
+    print("me: Do you have a specific genre in mind?")
+    print("you: [The genre is [name of the genre]]")
+    print("")
+    print("You can use [what can I do with [name of topic]] for certain topics to get more information about that topic. I can help you with the following topics:")
+    print("-[functions]: This shows you information about most of my commands.")
+    print("-[playback]:  This shows you information about my playback functions.")
+    print("-[volume]:    This shows you information about my volume functions.")
+    print("-[shuffle]:   This shows my shuffle options.")
+    print("-[repeat]:    This shows my repeat options.")
+    print("-[current]:   This shows my functions regarding your current playback.")
+    print("-[find]:      This shows information about how to use my find function.")
+    print("-[play]:      This shows information about how to use my play function.")
+    print("-[enqueue]:   This shows information about how to enqueue items to your queue.")
+    print("-[device]     This shows information about how to select your playback device. This however does not work always as good as I want but I blame Spotify for this.")
+    print("")
+    print("I can understand many versions of everything you tell me, but bear with me if I do not understand you right away.")
+    print("")
     print("Before you can use my full capabilities you first need to login.")
 
 
@@ -60,17 +80,28 @@ class SpotifySession():
   def help_functions(self):
     print("My main aim is to hold a conversation with you to assist you with Spotify.")
     print("If you are logged in I can control your Spotify. I can...")
-    print("- Play/Resume your Spotify.")
-    print("- Pause your Spotify.")
-    print("- Skip to the next track.")
-    print("- Go back to the previous track.")
-    print("- Rewind to the start of the track.")
-    print("- Turn shuffle on and off.")
-    print("- Set repeat to track, context or off.")
-    print("- Save the track you are currently listening to.")
-    print("- Find a track, album, artist or playlist for you. \n    You can then choose which item you want to play from a list of results.")
-    print("- Play a track, album, artist or playlist for you.")
-    print("- Tell you to which track you are currently listening.")
+    print("- [play] / [resume] / [pause] your Spotify.")
+    print("- [skip] to the [next track].")
+    print("- Go back to the [previous track].")
+    print("- [rewind] to the start of the track.")
+    print("- Turn [shuffle on] and [shuffle off].")
+    print("- Set [repeat] to track, context or off.")
+    print("- Ask for the [currently playing] track and ask [is current track saved].")
+    print("- You can then [add current to saved] or [remove current from saved]")
+    print("- I can [find track [name of track]], find you an album, artist or playlist in the same way.")
+    print("- I can also [play track [name of track]], or play an album, artist or playlist in the same way too.")
+    print("- You can also [enqueue [name of track]], however this only works with tracks.")
+    print("- Setting your playback device is also possible, but Spotify does not quite like that sometime.")
+    print("     This can be done by asking your [current device] or [select device]")
+    print("")
+    print("Each time I show you a list of items you can type [item [item number]] to select an item.")
+    print("You can often ask for [more items].")
+    print("You can also [query nextpage] or [query prevpage] to navigate the pages.")
+    print("")
+    print("Since I am a very sentimental bot, you can also talk about your feelings with me.")
+    print("I can also make you recomendations, this can be done in several ways.")
+    print("[can you make me a recommendation] is my preferred way of giving you recommendations.")
+    print("Or you can ask [play me a recommendation] if you want to listen to a recommendation right away.")
 
 
   def help_play_find(self):
@@ -84,6 +115,7 @@ class SpotifySession():
     print("- album")
     print("- artist")
     print("- playlist")
+    print("You can also enqueue items by typing [enqueue item [name of the item]], however this only works with tracks.")
 
 
   #########
@@ -456,7 +488,7 @@ class SpotifySession():
       Returns:
         - "PYOK PLAY [name of item] by [name of owner of item]" - Succesfully started playback of the requested item.
 	  '''
-    if index >= 0:
+    if index > 0:
       self._query_index = int(index) - 1 # Humans are 1-based.
     
     if self._query_index >= self._query_nresults:
@@ -497,7 +529,7 @@ class SpotifySession():
     if self._query_kind != "track":
       return "PYFAIL ENQUEUE INVALIDTYPE"
 
-    if index >= 0:
+    if index > 0:
       self._query_index = int(index) - 1
 
     track = self._query_results[self._query_index]
@@ -789,10 +821,9 @@ class SpotifySession():
     elif kind == "genre":
       possible_genres = self._sp.recommendation_genre_seeds()['genres']
       if query.strip() in possible_genres:
-        print('genre in possible genres')
         tracks = self._sp.recommendations(seed_genres=[query.strip()], limit=self._query_limit)
       else:
-        return "PYFAIL RECOMMEND NORESULTS"
+        return "PYFAIL RECOMMEND GENREUNKNOWN"
 
     elif kind == "track":
       found_track = self._sp.search(query.strip(), limit=1, type="track")
